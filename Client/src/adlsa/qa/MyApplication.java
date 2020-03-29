23fe4742c30581b27ac17a22de45ff7a1bc6f00f
IMK5 <imk.med5@gmail.com>
2020-03-29 08:24:34 +0300

    apdate

--- Client/src/adlsa/qa/MyApplication.java
+++ Client/src/adlsa/qa/MyApplication.java
@@ -10,17 +10,18 @@ public class MyApplication {
 
     public static void main(String[] args) {
         
-        String myDirectoryPath = "D:\\workspace\\test";
+        //String myDirectoryPath = "D:\\workspace\\test";
+        String myDirectoryPath="D:\\workspace\\test\\f1";
         File[] files = Service.getFilesFromDirectory(myDirectoryPath);
         System.out.println("\n *************  Actual Structure *************  ");
         //System.out.println("== YOU HAVE : [ "+ files.length + " ] FOLDERS");
         Service.showFiles(files);
-        System.out.println(" *************  Actual Structure ************* \n ");
+        System.out.println(" *************  END Actual Structure ************* \n ");
         System.out.println("============ Updating : ");
         try {
             Service.renameFile(myDirectoryPath);
-            System.out.println("============ After Updating ==== : ");
-            Service.showFiles(files);
+           /* System.out.println("============ After Updating ==== : ");
+            Service.showFiles(files);*/
         } catch (IOException e) {
             e.printStackTrace();
         }
